55f6cb2cf83f18c16d6468fc3eb486e61e76dd95
Meshal505 <rings-3@hotmail.com>
2019-04-03 16:22:57 +0300

    Updates

---
+++ precache-manifest.11c9400ea2fc37fd5ec0381bed06287d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a0c43bde80f322238324",
+    "url": "/react-gh-pages/static/js/runtime~main.ac2d5040.js"
+  },
+  {
+    "revision": "650750a4f1fd3a545167",
+    "url": "/react-gh-pages/static/js/main.223411cc.chunk.js"
+  },
+  {
+    "revision": "0f16420dcf309ef946d9",
+    "url": "/react-gh-pages/static/js/2.568e9868.chunk.js"
+  },
+  {
+    "revision": "650750a4f1fd3a545167",
+    "url": "/react-gh-pages/static/css/main.cebaf04f.chunk.css"
+  },
+  {
+    "revision": "c689c7dad9676278b58ac149a436913c",
+    "url": "/react-gh-pages/index.html"
+  }
+];
